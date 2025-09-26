e459ab879bcfb4695c9ee305ce3b5688501fd8ff
Adam <bielaa3@gmail.com>
2025-09-26 15:52:33 +0200

    aktualizacje

--- my_package/model_pricings.py
+++ my_package/model_pricings.py
@@ -1,31 +1,14 @@
 # SŁOWNIK SŁOWNIKÓW CENNIKI MODELI
 model_pricings = {
-    "gpt-4o": {  # NAJBARDZIEJ WSZECHSTRONNY MODEL OFEROWANY OPENAI
-        "input_tokens": 2.50 / 1_000_000,  # per token ZERA MOŻNA ROZDZIELAĆ W PYTONIE _
-        "output_tokens": 10.00 / 1_000_000,  # per token
-    },
-    "gpt-4o-mini": {  # SZYBSZA ZNACZNIE TAŃSZA WERSJA MODELU OFEROWANY OPENAI
-        "input_tokens": 0.150 / 1_000_000,  # per token
-        "output_tokens": 0.600 / 1_000_000,  # per token
-    },
-    "gpt-4.1-nano": {  # SZYBSZA ZNACZNIE TAŃSZA WERSJA MODELU OFEROWANY OPENAI
-        "input_tokens": 0.100 / 1_000_000,  # per token
-        "output_tokens": 0.400 / 1_000_000,  # per token
-    },
-    "gpt-4.1-mini": {  # SZYBSZA ZNACZNIE TAŃSZA WERSJA MODELU OFEROWANY OPENAI
-        "input_tokens": 0.400 / 1_000_000,  # per token
-        "output_tokens": 1.600 / 1_000_000,  # per token
-    },
-    "gpt-4.1": {  # SZYBSZA ZNACZNIE TAŃSZA WERSJA MODELU OFEROWANY OPENAI
-        "input_tokens": 2.00 / 1_000_000,  # per token
-        "output_tokens": 8.00 / 1_000_000,  # per token
-    },
-    "o4-mini": {  # Szybszy, bardziej przystępny model do rozumowania
-        "input_tokens": 1.1 / 1_000_000,  # per token
-        "output_tokens": 4.40 / 1_000_000,  # per token
-    },
-    "o3-mini": {  # Szybszy, bardziej przystępny model do rozumowania
-        "input_tokens": 1.1 / 1_000_000,  # per token
-        "output_tokens": 4.40 / 1_000_000,  # per token
-    }
+    "gpt-5-nano":   {"input_tokens": 0.050 / 1_000_000, "output_tokens": 0.400 / 1_000_000},
+    "gpt-4.1-nano": {"input_tokens": 0.100 / 1_000_000, "output_tokens": 0.400 / 1_000_000},
+    "gpt-4o-mini":  {"input_tokens": 0.150 / 1_000_000, "output_tokens": 0.600 / 1_000_000},
+    "gpt-4.1-mini": {"input_tokens": 0.400 / 1_000_000, "output_tokens": 1.600 / 1_000_000},
+    "gpt-5-mini":   {"input_tokens": 0.250 / 1_000_000, "output_tokens": 2.000 / 1_000_000},
+    "o3-mini":      {"input_tokens": 1.100 / 1_000_000, "output_tokens": 4.400 / 1_000_000},
+    "o4-mini":      {"input_tokens": 1.100 / 1_000_000, "output_tokens": 4.400 / 1_000_000},
+    "gpt-4.1":      {"input_tokens": 2.000 / 1_000_000, "output_tokens": 8.000 / 1_000_000},
+    "gpt-5":        {"input_tokens": 1.250 / 1_000_000, "output_tokens":10.000 / 1_000_000},
+    "gpt-5-chat-latest": {"input_tokens": 1.250 / 1_000_000, "output_tokens":10.000 / 1_000_000},
+    "gpt-4o":       {"input_tokens": 2.500 / 1_000_000, "output_tokens":10.000 / 1_000_000}
 }
